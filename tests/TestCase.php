846c82364215a0bc4f97b3f275c449712805a669
Matt Clinton <matt@laralabs.uk>
2017-09-11 15:41:51 +0100

    StyleCI amendments and edited travis config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,6 @@
 
 namespace Laralabs\Toaster\Tests;
 
-use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     /**
